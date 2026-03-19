1f9bab12814f00d47d15d4afa82e467aa60400af
daonguyenhp <giadao23112006@gmail.com>
2026-03-19 21:45:53 +0700

    Version 4.1

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const md5 = require('md5');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback" 
+    callbackURL: "https://icram-web.onrender.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
